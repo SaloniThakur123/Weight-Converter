b84022657a2f88c54fe4931f89d197e87c8ea89c
SaloniThakur123 <salonithakur.0109@gmail.com>
2023-10-09 20:24:38 +0530

    fix

--- WTconvertor.js
+++ WTconvertor.js
@@ -2,8 +2,8 @@ document.getElementById('output').style.visibility='hidden';
 document.getElementById('lbsinput').addEventListener('input',function(e){
     document.getElementById('output').style.visibility='visible';
     let lbs=e.target.value;
-    document.getElementById('gramsoutput').innerHTML=lbs/0.0022046;
-    document.getElementById('kgoutput').innerHTML=lbs/2.2046;
-    document.getElementById('ozoutput').innerHTML=lbs*16;
+    document.getElementById('gramsoutput').innerHTML=(lbs/0.0022046).toFixed(2);
+    document.getElementById('kgoutput').innerHTML=(lbs/2.2046).toFixed(2);
+    document.getElementById('ozoutput').innerHTML=(lbs*16).toFixed(2);
 
 });
